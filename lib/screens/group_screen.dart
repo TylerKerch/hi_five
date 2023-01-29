cc822973d04938fa1ace4306dbb625eba17d0eda
nathanhtkd <nathanhartojo@gmail.com>
2023-01-29 00:57:33 -0600

    added group screen woohoo

--- lib/screens/group_screen.dart
+++ lib/screens/group_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 class GroupScreen extends StatefulWidget {
   const GroupScreen({Key? key}) : super(key: key);
@@ -15,11 +16,50 @@ class GroupScreenState extends State<GroupScreen> {
 
   @override
   Widget build(BuildContext context) {
-
+    List<String> entries = <String>[
+      'Caleb',
+      'Aaron',
+      'Erik',
+      'Tyler',
+      'Nathan',
+      'Sean',
+      'Koki',
+      'Emily',
+      'Keisha',
+      'Cameron',
+      'Alexander',
+      'Austin',
+      'Kevin',
+      'Andrew'
+    ];
+    Random random = new Random();
+    var streaks = List<int>.generate(
+      entries.length,
+      (index) {
+        int num = random.nextInt(70);
+        return num;
+      },
+    );
     return Scaffold(
-        appBar: AppBar(title: Text("HiFive")),
-
-        body: Container()
+      appBar: AppBar(
+        backgroundColor: const Color(0xFF9ed4ff),
+        title: const Center(
+          child: Text('hiFive'),
+        ),
+      ),
+      body: ListView.separated(
+        itemCount: entries.length,
+        separatorBuilder: (context, index) {
+          return const Divider();
+        },
+        itemBuilder: (name, index) {
+          return ListTile(
+            leading: Icon(Icons.account_circle_sharp),
+            title: Text(entries[index]),
+            trailing: Text(streaks[index].toString() + " days"),
+          );
+        },
+      ),
     );
   }
 }
